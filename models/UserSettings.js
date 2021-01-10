4770847133f679432bc513d38a834ba8bdb600bc
SerdarSerifoglu <serdarserifoglu@gmail.com>
2021-01-10 21:30:59 +0300

    first

---
+++ models/UserSettings.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSettingsSchema = new Schema({
+  selectedPack: {
+    type: String,
+  },
+  wordColor: {
+    type: String,
+  },
+  userId: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("UserSettings", UserSettingsSchema);
